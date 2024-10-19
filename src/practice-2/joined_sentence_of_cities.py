ffa2cef16e752d2ed8f167c86fe2bd411ac38b70
Oleksandr Morozov <alex.mo@golaunchlabs.com>
2024-10-19 17:05:02 +0300

    add description for second practice

--- src/practice-2/joined_sentence_of_cities.py
+++ src/practice-2/joined_sentence_of_cities.py
@@ -1,3 +1,8 @@
+# 3. Дано список з такими елементами: cities = ['Budapest', 'Rome', 'Istanbul',
+# 'Sydney', 'Kyiv', 'Hong Kong']. Сформуйте з елементів списку повідомлення, у якому
+# перед останнім елементом буде вставлено слово and. Наприклад, у нашому випадку,
+# повідомлення буде таким: Budapest, Rome, Istanbul, Sydney, Kyiv and Hong
+# Kong. Програма має працювати з будь-якими списками, довжина яких є 6.
 cities = ['Budapest', 'Rome', 'Istanbul', 'Sydney', 'Kyiv', 'Hong Kong']
 formatted_cities = ', '.join(cities[:-1]) + ' and ' + cities[-1]
 print("Formatted cities list:", formatted_cities)
--- src/practice-2/reversed_digits.py
+++ src/practice-2/reversed_digits.py
@@ -1,3 +1,6 @@
+# 4. Необхідно зчитати рядок з 5 цифр, розділених пропусками, і зберегти кожну цифру у список.
+# Створіть копію списку із впорядкованими елементами у зворотному порядку. Виведіть число,
+# яке утворюється об’єднанням елементів нового списку.
 input_string = input("Enter a string of 5 digits separated by spaces: ")
 digits = input_string.split()
 if len(digits) != 5 or not all(digit.isdigit() for digit in digits):
